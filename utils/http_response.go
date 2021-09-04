31dc9eac29b699869e4982568f4614f5a3788776
Paulo <pauloeduardo2709@gmail.com>
2021-09-04 00:57:29 -0300

    improve: http response status code

--- utils/http_response.go
+++ utils/http_response.go
@@ -3,8 +3,7 @@ package utils
 import "github.com/gofiber/fiber/v2"
 
 func AsJSON(c *fiber.Ctx, status int, json fiber.Map) error {
-	c.Response().SetStatusCode(status)
-	return c.JSON(json)
+	return c.Status(status).JSON(json)
 }
 
 func AsError(c *fiber.Ctx, status int, msg string) error {
